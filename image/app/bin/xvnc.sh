c2cda75d72849b611c679ba879147a2004526d25
wn1980 <waipotn@hotmail.com>
2022-02-11 11:23:57 +0700

    change to display :9

--- image/app/bin/xvnc.sh
+++ image/app/bin/xvnc.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-rm -f /tmp/.X1-lock
+rm -f /tmp/.X11-unix/X9
+rm -f /tmp/.X9-lock
 
-Xvnc $DISPLAY -depth 24 -ac -pn -rfbport=5901 -SecurityTypes=None -desktop=RobotOps
+Xvnc :9 -depth 24 -ac -pn -rfbport=5901 -SecurityTypes=None -desktop=RobotOps
